b9b9a876d897a33e6e1f0373054bf1c1dd0823ec
unknown <ian.albuquerque.silva@gmail.com>
2018-04-22 05:42:12 -0300

    shop

---
+++ Assets/scripts/fps_clickers_script.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class fps_clickers_script : MonoBehaviour {
+
+
+	public float[] clickersCost;
+	public float[] clickersFPS;
+	
+	public Text[] amountTexts;
+	public Text[] costTexts;
+	public Text[] fpsTexts;
+	public Button[] buyButton;
+
+	public float[] clickerCount;
+	public int numClickers;
+
+	private float timeElapsed;
+
+	public float increaseInterval;
+	
+	public fps_resource_script fpsResourceScript;
+
+	// Use this for initialization
+	void Start () {
+		this.timeElapsed = 0.0f;
+		for(int i=0; i<this.numClickers; i++) {
+			this.costTexts[i].text = "Costs " + this.clickersCost[i] + " Froots";
+			this.fpsTexts[i].text = "+" + this.clickersFPS[i] + " FPS";
+			this.amountTexts[i].text = "x" + this.clickerCount[i] + " Adopted";
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.timeElapsed += Time.deltaTime;
+		if (this.timeElapsed > this.increaseInterval) {
+			for(int i=0; i<this.numClickers; i++) {
+				this.fpsResourceScript.addToCurrentF(this.clickersFPS[i] * this.increaseInterval * this.clickerCount[i]);
+			}
+			this.timeElapsed = 0.0f;
+		}
+
+	}
+}
---
+++ Assets/scripts/fps_shop_game.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class fps_shop_game : MonoBehaviour {
+
+	public Button buttonGotoShop;
+	public Button buttonGotoGame;
+
+	public Canvas gameCanvas;
+	public Canvas shopCanvas;
+
+	// Use this for initialization
+	void Start () {
+		this.gameCanvas.enabled = true;
+		this.shopCanvas.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.buttonGotoShop.onClick.AddListener(onClickGotoShop);
+		this.buttonGotoGame.onClick.AddListener(onClickGotoGame);
+	}
+
+	void onClickGotoShop()
+	{
+		this.gameCanvas.enabled = false;
+		this.shopCanvas.enabled = true;
+	}
+
+	void onClickGotoGame()
+	{
+		this.gameCanvas.enabled = true;
+		this.shopCanvas.enabled = false;
+	}
+}
